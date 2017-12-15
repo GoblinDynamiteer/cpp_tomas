0e6f36ccd3a91c6668b7a8aa143aa825298559eb
Johan Kämpe <kampe.johan@gmail.com>
2017-12-15 09:34:01 +0100

    Added another func macro

--- func_macro/func.c
+++ func_macro/func.c
@@ -3,19 +3,23 @@
 #include <stdio.h>
 
 #define INC_ONE_MAX(v, m) (v + 1 > m ? m : v + 1)
+#define DEC_ONE_MIN(v, m) (v - 1 < m ? m : v - 1)
 
 int main(int argc, char const *argv[])
 {
-    int x = 1;
+    int x = 1 , y = 30;
 
     printf("x before loop: %d\n", x);
+    printf("y before loop: %d\n", y);
 
     for (size_t i = 0; i < 300; i++)
     {
         x = INC_ONE_MAX(x, 20);
+        y = DEC_ONE_MIN(y, 14);
     }
 
     printf("x after loop: %d\n", x);
+    printf("y after loop: %d\n", y);
 
     return 0;
 }
