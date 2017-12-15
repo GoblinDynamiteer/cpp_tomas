248a23f4f4740826084d6790b4b46761b243effd
Johan Kämpe <kampe.johan@gmail.com>
2017-12-15 09:56:44 +0100

    Added namespace and using

--- namespace/myspace.cpp
+++ namespace/myspace.cpp
@@ -11,11 +11,23 @@ namespace space_one
     }
 }
 
+namespace space_two
+{
+    int sub(int a, int b)
+    {
+        return a - b;
+    }
+}
+
+
+using namespace space_two;
+
 int main()
 {
     int x = 2, y = 1;
 
     cout << x << "+" << y << "=" << space_one::add(x, y) << endl;
+    cout << x << "-" << y << "=" << sub(x, y) << endl;
 
     return 0;
 }
