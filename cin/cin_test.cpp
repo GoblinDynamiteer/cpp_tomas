fedd8480505448f0e6dcd763858fc99e79f21366
Johan Kämpe <kampe.johan@gmail.com>
2017-12-15 09:40:28 +0100

    cin test

---
+++ cin/cin_test.cpp
@@ -0,0 +1,16 @@
+/* cin test */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "Enter integer value:";
+
+    cin >> x;
+    cout << "You entered: " << x << endl;
+
+    return 0;
+}
